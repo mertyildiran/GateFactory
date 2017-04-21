427de8082291b5bfe27cc8a17bfaadd186bb4a73
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-04-21 13:07:45 +0300

    Add NAND(), rbl() functions and Plug class

--- gate/gate.py
+++ gate/gate.py
@@ -1,18 +1,23 @@
 import math
 import threading
+import random
+import itertools
 
-class NAND():
+class Plug():
 
-	def __init__(self,factory):
+	def __init__(self):
 		pass
 
 
 class Factory():
 
-	def __init__(self,input_dim=0,output_dim=0):
-		self.input = []
-		self.target = []
+	def __init__(self,input_dim=0,output_dim=0,threshold=0.5):
+		self.input = {}
+		for element in self.rbl(input_dim):
+			self.input[Plug()] = element
+		self.target = self.rbl(output_dim)
 		self.output = []
+		self.threshold = threshold
 
 		self.mini_batch = []
 		self.mini_batch_limit = int(math.sqrt(input_dim))
@@ -21,7 +26,10 @@ class Factory():
 
 		self.stopper = False
 		self.thread = None
-		self.start()
+		#self.start()
+
+	def test(self):
+		return list(itertools.combinations_with_replacement(self.input,2))
 
 	def _start(self):
 		pass
@@ -44,7 +52,13 @@ class Factory():
 			print "Size of the input of the factory: " + str(len(self.input))
 			print "These values are not matching! Please fix it and try it again."
 		else:
-			self.input = input_arr
+			step = 0
+			for element in input_arr:
+				if element > self.threshold:
+					self.input[step] = 1
+				else:
+					self.input[step] = 0
+				step += 1
 		if output_arr is None:
 			self.mini_batch = []
 			self.target = []
@@ -58,3 +72,10 @@ class Factory():
 				self.mini_batch.append([input_arr,output_arr])
 				if len(self.mini_batch) > self.mini_batch_limit:
 					self.mini_batch.pop(0)
+
+	def NAND(self,duple):
+		return not (self.input[duple[0]] and self.input[duple[1]])
+
+	#random binary list
+	def rbl(self,n):
+		return [random.randint(0,1) for b in range(1,n+1)]
